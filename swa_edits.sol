ca3309e9a0562bf0d4a44abb9b8aa944724a4401
Lam <Herman.Lam@ontario.ca>
2018-01-16 10:43:46 -0500

    update2

--- swa_edits.sol
+++ swa_edits.sol
@@ -17,6 +17,7 @@ pragma solidity ^0.4.15;
 import './ERC20.sol';
 import './SafeMath.sol';
 import './LoggingErrors.sol';
+import './Token.sol';
 
 /**
  * @title Standard ERC20 Token
@@ -37,19 +38,28 @@ contract SWA is ERC20, LoggingErrors {
   uint256 public totalSupply_;
 
   enum participantType {IO, CLIENT, CBRE, CONSULTANT, VENDOR }
-  enum contractStatus { CREATED, INITIATED, WORKSTART, INSUFFICENTFUND , COMPLETED}
+
+  // planning when consultant is on board.
+  // initiated is client and cbre is set.
+  // EXECUTION is when the vendor is on board and budget check is good.
+  // CLOSEOUT when everyone is approved.
+  // PARTIAL PAYOUT when money is partially pay.
+  enum contractStatus { CREATED, INITIATED, PLANNING, EXECUTION, INSUFFICENTFUND , CLOSEOUT, PARTIAL_PAYOUT, PAYOUT, COMPLETE}
   
   address owner_;
 
   struct Participant {
     address id; //account number that will send and/or receive tokens
     uint receivable; // how much will this participant receive when the contract is done
-    ERC20 wallet;
     bool approved;
+    uint apprvoedTime;
     bool paid; // once it is all approved, we will pay the participants from contract balance and will mark as paid here
   participantType role;
   
   }
+
+
+  
 
   // create a contract without participants
   // add participants later
@@ -74,6 +84,7 @@ contract SWA is ERC20, LoggingErrors {
    * Events
    */
   event LogTokensMinted(address indexed _to, address to, uint256 value, uint256 totalSupply);
+  event logContractStatusChangeEvent( uint contractId, contractStatus value);
 
   /**
    * @dev CONSTRUCTOR - set token owner account
@@ -113,6 +124,7 @@ function addParticipant(uint _contractId, address _id, participantType role) ext
           contracts[_contractId].participants[_id] = participant;
           contracts[_contractId].participantIndex[lastParticipant] = participant;
          
+           changeContractStatus(_contractId, contractStatus.INITIATED);
         }
         
         // Only CBRE is allowed to add Consultant and Vendor Role
@@ -127,7 +139,10 @@ function addParticipant(uint _contractId, address _id, participantType role) ext
             participant.approved=false;
             contracts[_contractId].participants[_id] = participant;    
             contracts[_contractId].participantIndex[lastParticipant] = participant;  
-           
+            if (contracts[_contractId].numParticipants == 5 ) {
+            
+            
+            }
           } 
        }
    }
@@ -170,9 +185,11 @@ function budgetCheckFail(uint _contractId) returns (bool) {
     
               // check if the contract balance < consultant fee + vendorCost.
               if ( contracts[_contractId].balance < (vendorAmt + consultantAmt) ) {
-                  contracts[_contractId].status =contractStatus.INSUFFICENTFUND; 
+                  changeContractStatus(_contractId, contractStatus.INSUFFICENTFUND);
+        
                   return true;
-              }
+
+              } 
         }
         return false;
 
@@ -187,6 +204,11 @@ function admininstratorSetContractFunds(uint _contractId, address participantId,
          if ( contracts[_contractId].participants[participantId].role == participantType.CONSULTANT  || contracts[_contractId].participants[participantId].role == participantType.VENDOR )  {
 
               contracts[_contractId].participants[participantId].receivable = amt;
+
+              if ( contracts[_contractId].participants[participantId].role ==   participantType.CONSULTANT ) {
+                changeContractStatus(_contractId, contractStatus.PLANNING);
+                 
+              }
               budgetCheckFail(_contractId);
 
              
@@ -203,24 +225,32 @@ function setParticipantResponse(uint _contractId,  bool approve) external return
      
      // sender set their own approval status.
      contracts[_contractId].participants[msg.sender].approved = approve;
-     // if vendor set contract to approve, trigger event notification to other participants
-     if (  contracts[_contractId].participants[msg.sender].role == participantType.VENDOR) {
-          contracts[_contractId].status = contractStatus.COMPLETED;
+     contracts[_contractId].participants[msg.sender].apprvoedTime=block.timestamp;
+     // do a payout check
+     partialPayout(_contractId);
+        
 
-       // logContractStatusEvent(contractStatus.COMPLETED);
-     }
 
   return true;
 }
+
+function   changeContractStatus(uint _contractId, contractStatus statusToBe) private returns (bool) {
+  
+    if ( checkValidTransistionState( _contractId, statusToBe) ) {
+         contracts[_contractId].status = statusToBe;
+         logContractStatusChangeEvent(_contractId, statusToBe);
+         return true;
+    }
+    return false;
+} 
 
 function ceil(uint a, uint m) constant returns (uint ) {
         return ((a + m - 1) / m) * m;
 }
     
-function payout(uint _contractId) {
+function partialPayout(uint _contractId) private returns (bool) {
   // Only CBRE do the payout.
-  if ( contracts[_contractId].participants[msg.sender].role ==participantType.CBRE ) {
-   
+ 
     // Ensure all the particpants are there before we check.
       if ( contracts[_contractId].numParticipants == 5 ) {
           uint numApprovedParty = 0;      
@@ -229,28 +259,89 @@ function payout(uint _contractId) {
               if ( contracts[_contractId].participantIndex[i].approved == true ) {
                 numApprovedParty++;
                   if ( numApprovedParty == contracts[_contractId].numParticipants) {
+
+                      // Change contract status to CLOSEOUT
+                      changeContractStatus(_contractId, contractStatus.CLOSEOUT); 
+
                       // payout to Vendor and consultant from CBRE wallet.
                       Participant memory cbre=findParticipantByRole( _contractId, participantType.CBRE);
                       Participant memory vendor=findParticipantByRole( _contractId, participantType.VENDOR);
-               //     uint vendorAmt=ceil(vendor.receivable * 0.9, 10);
-                      uint vendorAmt=ceil(vendor.receivable, 10);
-                      cbre.wallet.transfer(vendor.id,vendorAmt ); 
+               //     
+
+                      // 90% of num, 2 decimals
+                      uint vendorAmt = vendor.receivable * 90/100;
+
+                      Token token=Token(cbre.id);
+                      token.mint( cbre.id,cbre.receivable);
+
+                      token.transfer(vendor.id, vendorAmt ); 
                       vendor.paid = true;
         
                       Participant memory consultant=findParticipantByRole( _contractId, participantType.CONSULTANT);
-                      cbre.wallet.transfer(consultant.id, consultant.receivable); 
-        
+                      token.transfer(consultant.id, consultant.receivable); 
+                      changeContractStatus(_contractId, contractStatus.PARTIAL_PAYOUT); 
+
+                      return true;
                   }      
              }
    
            } 
-    }
+   
+    return false;
 
+}
+
+
+  
+function checkValidTransistionState(uint _contractId, contractStatus statusToBe) private returns (bool) {
+
+    // In order to be closed, the contract need to be fully payout.
+    if ( contractStatus.COMPLETE == statusToBe &&
+       contracts[_contractId].status == contractStatus.PAYOUT ) {
+       return true;
+    } else 
+    if ( contractStatus.PAYOUT == statusToBe &&
+     contracts[_contractId].status == contractStatus.PARTIAL_PAYOUT ) {
+       return true;
+    } else
+     if ( contractStatus.PARTIAL_PAYOUT == statusToBe &&
+     contracts[_contractId].status == contractStatus.CLOSEOUT ) {
+       return true;
+    } else
+     if ( contractStatus.CLOSEOUT == statusToBe &&
+     contracts[_contractId].status == contractStatus.EXECUTION ) {
+       return true;
+    } else
+    if ( contractStatus.EXECUTION == statusToBe &&
+     contracts[_contractId].status == contractStatus.INSUFFICENTFUND ) {
+       return true;
+    } else
+    if ( contractStatus.INSUFFICENTFUND == statusToBe &&
+     contracts[_contractId].status == contractStatus.EXECUTION ) {
+       return true;
+    } else
+    if ( contractStatus.EXECUTION == statusToBe &&
+     contracts[_contractId].status == contractStatus.PLANNING ) {
+       return true;
+    } else
+   if ( contractStatus.PLANNING == statusToBe &&
+     contracts[_contractId].status == contractStatus.INITIATED ) {
+       return true;
+    } else
+   if ( contractStatus.INITIATED == statusToBe &&
+     contracts[_contractId].status == contractStatus.CREATED ) {
+       return true;
+    } else
+      return false;
 }
 
 function closeContract (uint _contractId) external returns (bool) {
 
-  return true;
+  if ( checkValidTransistionState( _contractId, contractStatus.COMPLETE ) ) {
+     contracts[_contractId].status =contractStatus.COMPLETE;
+      return true;
+  } else
+      return false;
 }
 
 }
